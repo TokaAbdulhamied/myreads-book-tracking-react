90eecc15281be1fc28b98129d946d7a9e157ffc5
TokaAbdulhamied <tokaabdulhamied@gmail.com>
2021-09-21 20:37:54 +0200

    add search handlers

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Book from './Book'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import * as BooksApi from '../BooksAPI'
 
-function Search() {
+function Search({books:books, update:update}) {
+
+  const [search, setsearch] = useState("")
+   const [edited, setEdited] = useState([])
+   const [notfound, setNotfound] = useState(false)
+
+  const handleChange =(e)=>{
+      setsearch(e.target.value)
+  }
+  const handleKeyPress= (e) =>{
+    if (e.key === 'Enter' && search !== ""){
+
+      BooksApi.search (search).then ((results)=>{
+        setNotfound (false)
+         setEdited (results.map ((book)=>{
+
+          book = {...book, shelf:"none"}
+          books.filter ((saved)=>saved.id === book.id).
+          map (edit => book.shelf = edit.shelf)
+          return book
+
+        }))
+
+          console.log (edited)
+      }).catch ((e)=>{
+          setNotfound (true)
+      })
+      
+    }
+  }
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -14,22 +46,30 @@ function Search() {
         <button className="close-search" >Close</button>
         </Link>
         <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
+          <input type="text" placeholder="Search by title or author"
+          
+          onKeyPress={handleKeyPress}
+          value={search}
+          onChange={handleChange}
+          />
 
         </div>
       </div>
       <div className="search-books-results">
+        {notfound ?
+        <h1>No Results Found </h1>:
+        
         <ol className="books-grid">
-
+        { 
+               edited.map ((i, index)=>(
+              <li key={index}>
+              <Book book={i} update={update}></Book>
+              </li>
+              ))
+            }
         </ol>
+}
       </div>
     </div>
   )
